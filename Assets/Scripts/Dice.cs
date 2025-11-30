13c1accccfafecc4d4cf5f1c7db7b4ffaf07931c
SanazSobhanloo <cnzsg1@gmail.com>
2025-12-01 00:24:52 +0330

    feat: defining variables

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Dice : MonoBehaviour
+{
+    AudioManager audioManager;
+    public AudioClip diceRollingSound;
+
+    [Header("Dice Settings")]
+    public Sprite[] diceSides;         // 6 تصویر
+    public int[] diceValues = new int[6]; // 6 مقدار متناظر
+    public Image diceImage;
+
+    [Header("Roll FX")]
+    public float rollDuration = 1f;
+    public float rollSpeed = 0.05f;
+
+    public event Action<int> OnDiceRolled;
+
+    private bool isRolling = false;
+
+    public PlayerController currentPlayer;
+
+}
