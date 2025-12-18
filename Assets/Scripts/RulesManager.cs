bf46dc0c6cba260c192107c77c3640beef7c8a6d
zahrasiyahkalzade <zahra.siyahkan.zade@gmail.com>
2025-12-18 09:41:04 -0800

    creata RULESmanager

--- Assets/Scripts/RulesManager.cs
+++ Assets/Scripts/RulesManager.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class RulesManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   
+     [Header("Win")]
+    public WinManager winManager;
+
+       // ---------- Finish Bays ----------
+    [System.Serializable]
+    public class FinishBay
     {
-        
+        public PlayerColor color;
+        public List<Transform> slots = new List<Transform>(); // ترتیب اسلات‌ها
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Finish Bays (assign in Inspector)")]
+    public List<FinishBay> finishBays = new List<FinishBay>();
+
+    // ---------- Internal State ----------
+    private readonly HashSet<Token> finishedTokens = new HashSet<Token>();
+    private readonly Dictionary<Token, int> homeSlotOfToken = new Dictionary<Token, int>();
+
+    private readonly Dictionary<PlayerColor, int> finishCounters = new Dictionary<PlayerColor, int>();
 }
