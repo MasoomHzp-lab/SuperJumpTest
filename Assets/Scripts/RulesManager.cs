a6e01310a445e3c690c89f7d7796470364cfcc4a
zahrasiyahkalzade <zahra.siyahkan.zade@gmail.com>
2025-12-18 09:57:28 -0800

    add some scripts

--- Assets/Scripts/RulesManager.cs
+++ Assets/Scripts/RulesManager.cs
@@ -1,25 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RulesManager : MonoBehaviour
 {
-   
-     [Header("Win")]
-    public WinManager winManager;
+    
 
-       // ---------- Finish Bays ----------
-    [System.Serializable]
-    public class FinishBay
-    {
-        public PlayerColor color;
-        public List<Transform> slots = new List<Transform>(); // ترتیب اسلات‌ها
-    }
-
-    [Header("Finish Bays (assign in Inspector)")]
-    public List<FinishBay> finishBays = new List<FinishBay>();
-
-    // ---------- Internal State ----------
-    private readonly HashSet<Token> finishedTokens = new HashSet<Token>();
-    private readonly Dictionary<Token, int> homeSlotOfToken = new Dictionary<Token, int>();
-
-    private readonly Dictionary<PlayerColor, int> finishCounters = new Dictionary<PlayerColor, int>();
 }
+
+
+
