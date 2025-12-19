e4ac46959c314bf43a0bead9b7fbae83cfb3bc92
SanazSobhanloo <cnzsg1@gmail.com>
2025-12-19 19:18:26 +0330

    feat: add CheatTapZone structure and configuration fields

---
+++ Assets/Scripts/CheatTapZone.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class CheatTapZone : MonoBehaviour, IPointerDownHandler
+{
+    [Range(1, 6)] public int forcedValue = 6;
+    public int tapsRequired = 3;
+    public float multiTapWindow = 0.6f;
+    public float forceTimeoutSeconds = 3f;
+
+    private int tapCount = 0;
+    private float lastTapTime = -10f;
+
+    private void Reset()
+    {
+        var img = GetComponent<Image>();
+        if (img != null)
+        {
+            var c = img.color; c.a = 0f; img.color = c;
+            img.raycastTarget = true;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // منطق در کامیت بعدی اضافه می‌شود
+    }
+}
