8301c0843e5bd27289559bf5dd0e52e2f1d79e53
SanazSobhanloo <cnzsg1@gmail.com>
2025-12-19 19:19:07 +0330

    feat: implement multi-tap detection and CheatManager integration

--- Assets/Scripts/CheatTapZone.cs
+++ Assets/Scripts/CheatTapZone.cs
@@ -1,30 +1,15 @@
-using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
-
-[RequireComponent(typeof(RectTransform))]
-public class CheatTapZone : MonoBehaviour, IPointerDownHandler
+public void OnPointerDown(PointerEventData eventData)
 {
-    [Range(1, 6)] public int forcedValue = 6;
-    public int tapsRequired = 3;
-    public float multiTapWindow = 0.6f;
-    public float forceTimeoutSeconds = 3f;
+    if (Time.unscaledTime - lastTapTime > multiTapWindow)
+        tapCount = 0;
 
-    private int tapCount = 0;
-    private float lastTapTime = -10f;
+    tapCount++;
+    lastTapTime = Time.unscaledTime;
 
-    private void Reset()
+    if (tapCount >= tapsRequired)
     {
-        var img = GetComponent<Image>();
-        if (img != null)
-        {
-            var c = img.color; c.a = 0f; img.color = c;
-            img.raycastTarget = true;
-        }
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        // منطق در کامیت بعدی اضافه می‌شود
+        tapCount = 0;
+        if (CheatManager.Instance != null)
+            CheatManager.Instance.ForceNextRoll(forcedValue, forceTimeoutSeconds);
     }
 }
