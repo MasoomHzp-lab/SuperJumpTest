74686e71acf922c8ab7304b0e44c05eeb8ee3f07
Masoom_Hzp <susieshooter@gmail.com>
2025-12-28 20:05:18 +0330

    add buttons script

---
+++ Assets/Scripts/ButtonsScripts.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsScripts : MonoBehaviour
+{
+     public GameObject Panel;
+
+    public void OpenPannel(GameObject panel)
+    {
+
+        panel.SetActive(true);
+
+    }
+
+    public void ClosePanel(GameObject panel)
+    {
+
+        panel.SetActive(false);
+
+    }
+
+    
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+     public void PlaySoundButton()
+    {
+
+        AudioManager.Instance.PlayButtonSound();
+    }
+}
