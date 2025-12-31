7b050157c232519121ed7408f26a5c5a8e56da6c
zahrasiyahkalzade <zahra.siyahkan.zade@gmail.com>
2025-12-31 22:45:23 +0330

    feat OnClickSelectTheme method

--- Assets/Scripts/ThemeButton.cs
+++ Assets/Scripts/ThemeButton.cs
@@ -7,5 +7,13 @@ public class ThemeButton : MonoBehaviour
     [Header("Paid settings (for future)")]
     public bool isPaidTheme = false; 
     public string productId;   // اسم محصول در بازار، بعدا پرش می‌کنیم
+ public void OnClickSelectTheme()
+    {
+        if (ThemeManager.I == null)
+        {
+            Debug.LogWarning("[ThemeButton] ThemeManager not found.");
+            return;
+        }
 
+        // اگر باز شده → انتخابش کن
 }
