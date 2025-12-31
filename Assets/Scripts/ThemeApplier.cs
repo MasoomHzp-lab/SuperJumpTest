6b1a699711625a2ab33b2673e20a72f514359651
zahrasiyahkalzade <zahra.siyahkan.zade@gmail.com>
2025-12-31 21:57:05 +0330

    create ThemeApplier

---
+++ Assets/Scripts/ThemeApplier.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class ThemeApplier : MonoBehaviour
+{
+    [Header("Where to apply board sprite")]
+    public SpriteRenderer boardRenderer;  // اختیاری، اگه خالی باشه خودش از همین آبجکت می‌گیره
+}
