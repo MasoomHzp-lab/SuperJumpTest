1c6f0e5d1dcccb66bedbedac341922636d878a44
zahrasiyahkalzade <zahra.siyahkan.zade@gmail.com>
2025-12-31 22:06:26 +0330

    feat Awake method

--- Assets/Scripts/ThemeApplier.cs
+++ Assets/Scripts/ThemeApplier.cs
@@ -4,4 +4,11 @@ public class ThemeApplier : MonoBehaviour
 {
     [Header("Where to apply board sprite")]
     public SpriteRenderer boardRenderer;  // اختیاری، اگه خالی باشه خودش از همین آبجکت می‌گیره
+    private void Awake()
+    {
+        // اگر یادت رفت دستی ست کنی، خودش می‌ره روی همون آبجکت دنبال SpriteRenderer می‌گرده
+        if (boardRenderer == null)
+            boardRenderer = GetComponent<SpriteRenderer>();
+    }
+
 }
