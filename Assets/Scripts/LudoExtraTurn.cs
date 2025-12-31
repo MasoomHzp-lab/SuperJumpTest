a616eb4cf8b879b1588b9f518d155fdf1ab6b32a
fatemeashit <fatemashit82@gmail.com>
2025-12-31 20:07:13 +0330

    Add state tracking for extra turn logic

---
+++ Assets/Scripts/LudoExtraTurn.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LudoExtraTurn : MonoBehaviour
+{
+    [Header("References")]
+    public Dice dice;
+    public List<PlayerController> players = new List<PlayerController>();
+
+    [Header("Settings")]
+    public bool enabledExtraTurn = true;
+    private bool lastRollWasSix = false;
+    private readonly Dictionary<PlayerController, bool> movingMap = new Dictionary<PlayerController, bool>();
+    private PlayerController snapshotPlayerOnRoll;
+
+}
